dbcd7b2140157b76d1008b2d7130d3162fe437a4
Safin Singh <safin.singh@gmail.com>
2020-08-15 11:44:16 -0700

    finalize everything

--- build/logger.js
+++ build/logger.js
@@ -1,18 +1,1 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.success = exports.info = exports.warn = exports.fail = void 0;
-var chalk_1 = require("chalk");
-var constants_1 = require("./constants");
-exports.fail = function (message) {
-    constants_1.log(chalk_1.red("[-] " + message));
-    process.exit(1);
-};
-exports.warn = function (message) {
-    constants_1.log(chalk_1.yellow("[!] " + message));
-};
-exports.info = function (message) {
-    constants_1.log(chalk_1.blue("[*] " + message));
-};
-exports.success = function (message) {
-    constants_1.log(chalk_1.green("[+] " + message));
-};
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.success=exports.info=exports.warn=exports.fail=void 0;var chalk_1=require("chalk"),constants_1=require("./constants");exports.fail=function(s){constants_1.log(chalk_1.red("[-] "+s)),process.exit(1)},exports.warn=function(s){constants_1.log(chalk_1.yellow("[!] "+s))},exports.info=function(s){constants_1.log(chalk_1.blue("[*] "+s))},exports.success=function(s){constants_1.log(chalk_1.green("[+] "+s))};
